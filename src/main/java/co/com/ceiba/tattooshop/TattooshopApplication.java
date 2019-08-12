07c572c3cb86d277ba0b655201d4e2745e873776
Alejandro Rueda Duarte - Ceiba Software <alejandro.rueda@ceiba.com.co>
2019-08-12 10:30:03 -0500

    commit inicial

---
+++ src/main/java/co/com/ceiba/tattooshop/TattooshopApplication.java
@@ -0,0 +1,13 @@
+package co.com.ceiba.tattooshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TattooshopApplication.class, args);
+	}
+
+}
