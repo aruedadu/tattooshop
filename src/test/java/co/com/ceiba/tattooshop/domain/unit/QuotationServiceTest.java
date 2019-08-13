45d884665fe41fe018fd26cfb1cb0ad11dbae623
Alejandro Rueda Duarte - Ceiba Software <alejandro.rueda@ceiba.com.co>
2019-08-13 16:50:00 -0500

    sonar properties updated

---
+++ src/test/java/co/com/ceiba/tattooshop/domain/unit/QuotationServiceTest.java
@@ -0,0 +1,14 @@
+package co.com.ceiba.tattooshop.domain.unit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class QuotationServiceTest {
+	
+	@Test
+	public void createQuotationTest() {
+		assertEquals((2+2), 4);		
+	}
+
+}
