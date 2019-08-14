e9aa45f14184169ece8d6dff159de646575e194a
Alejandro Rueda Duarte - Ceiba Software <alejandro.rueda@ceiba.com.co>
2019-08-14 14:54:30 -0500

    rm code smells

--- src/main/java/co/com/ceiba/tattooshop/domain/service/AppointmentService.java
+++ src/main/java/co/com/ceiba/tattooshop/domain/service/AppointmentService.java
@@ -5,23 +5,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class AppointmentService {
 	
-	public void testMethodSonar(String a, String b, String c, String s) throws RuntimeException{
-		if(a==b) {
-			System.err.println("oe");
-			if(b==c) {
-				if(c==s) {
-					if(s==a) {
-						if(s==b) {
-							if(c==a) {
-								if(a==s) {
-									throw new RuntimeException("oelo");
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
+	
 }
