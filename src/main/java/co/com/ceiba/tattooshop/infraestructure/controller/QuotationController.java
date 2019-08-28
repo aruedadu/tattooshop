4dddbf4eeb79a5c019c6edb2423a2dbc2d3c3c89
Alejandro Rueda Duarte - Ceiba Software <alejandro.rueda@ceiba.com.co>
2019-08-28 14:06:27 -0500

    correccion de fechas

--- src/main/java/co/com/ceiba/tattooshop/infraestructure/controller/QuotationController.java
+++ src/main/java/co/com/ceiba/tattooshop/infraestructure/controller/QuotationController.java
@@ -1,9 +1,12 @@
 package co.com.ceiba.tattooshop.infraestructure.controller;
 
 import java.time.ZoneId;
+import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalField;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +17,7 @@ import co.com.ceiba.tattooshop.domain.model.Quotation;
 import co.com.ceiba.tattooshop.domain.service.QuotationService;
 import co.com.ceiba.tattooshop.infraestructure.controller.peticiones.CotizacionRequest;
 
+@CrossOrigin
 @RestController
 @RequestMapping(value = "/tattooshop/cotizacion", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class QuotationController {
@@ -24,7 +28,18 @@ public class QuotationController {
 	@PostMapping(value = "/generar")
 	public @ResponseBody Quotation consultarCotizacion(@RequestBody CotizacionRequest payload) {
 		return servicio.getQuotation(
-				payload.getFechaInicio().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(),
+				payload.getFechaInicio().toInstant().atZone(ZoneId.from(new TemporalAccessor() {
+					
+					@Override
+					public boolean isSupported(TemporalField field) {
+						return true;
+					}
+					
+					@Override
+					public long getLong(TemporalField field) {
+						return 0;
+					}
+				})).toLocalDateTime(),
 				payload.getDuracion());
 
 	}
--- src/main/java/co/com/ceiba/tattooshop/infraestructure/controller/peticiones/CotizacionRequest.java
+++ src/main/java/co/com/ceiba/tattooshop/infraestructure/controller/peticiones/CotizacionRequest.java
@@ -3,15 +3,12 @@ package co.com.ceiba.tattooshop.infraestructure.controller.peticiones;
 import java.io.Serializable;
 import java.util.Date;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-
 public class CotizacionRequest implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 2227360870668674891L;
-	@JsonFormat(pattern = "yyyy-MM-dd HH:mm")
 	private Date fechaInicio;
 	private int duracion;
 
